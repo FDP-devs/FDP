3368ae5f14cfcb4a8902f5726babc6b574ccc435
glenn-syj <glenn.syj@gmail.com>
2025-01-09 23:43:05 +0900

    feat: 글로벌 유효성 검사 파이프 추가
    - ValidationPipe를 사용하여 DTO에 정의되지 않은 속성을 제거하고, 비허용 속성에 대한 에러 발생
    - 타입 변환 활성화로 데이터 유효성 검사 및 변환 기능 강화

--- FDP/backend/src/main.ts
+++ FDP/backend/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // DTO에 정의되지 않은 속성 제거
+      forbidNonWhitelisted: true, // DTO에 정의되지 않은 속성이 있으면 에러
+      transform: true, // 타입 변환 활성화
+    })
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
