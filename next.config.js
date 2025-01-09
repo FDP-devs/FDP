71973aeecde17745e6f8bf569688d004d30edddc
hd2908 <im2908@naver.com>
2025-01-09 23:55:26 +0900

    feat: 모달 컴포넌트 및 컨텍스트 추가, TypeScript 경로 설정 개선

--- next.config.js
+++ next.config.js
@@ -1,20 +1,15 @@
 const path = require('path');
 
 module.exports = {
-  webpack: (config, { isServer }) => {
-    // 예시: 특정 모듈을 alias로 설정
-    config.resolve.alias['@components'] = path.join(__dirname, 'components');
-
-    // 서버 전용 설정
-    if (isServer) {
-      // 서버 전용 Webpack 설정
-    }
-
-    // 클라이언트 전용 설정
-    if (!isServer) {
-      // 클라이언트 전용 Webpack 설정
-    }
-
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.join(__dirname, './'),
+      '@components': path.join(__dirname, 'components'),
+      '@contexts': path.join(__dirname, 'contexts'),
+      '@types': path.join(__dirname, 'types'),
+      '@styles': path.join(__dirname, 'styles')
+    };
     return config;
   },
 };  
